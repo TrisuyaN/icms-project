53e0cefc0f10636003c84e009ed75384c7345f2f
TrisuyaN <csy.2003@outlook.com>
2024-08-29 04:36:09 +0800

    sc-backend：更新 employeeinfo 表对应的 Employee DAO 实现；引入 MPJ 依赖并重构 EmployeeDao

--- sc-backend/src/main/java/com/sc/scbackend/enums/EmployeePosition.java
+++ sc-backend/src/main/java/com/sc/scbackend/enums/EmployeePosition.java
@@ -9,9 +9,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum EmployeePosition implements IEnum<String> {
-    SUPER_ADMINISTRATOR("Super Administrator", "超级管理员"),
-    SECURITY_GUARD("Security Guard", "安保人员"),
-    CLEANER("Cleaner", "保洁人员");
+    SUPER_ADMINISTRATOR("超级管理员", "超级管理员"),
+    SECURITY_GUARD("保安", "安保人员"),
+    CLEANER("保洁", "保洁人员");
 
     private final String value;
     private final String desc;

c3f3f0be632fcb7a6009180861ac689ae5724968
TrisuyaN <csy.2003@outlook.com>
2024-09-04 01:44:36 +0800

    sc-backend：更新 /sc/api/gasleak/findall 和 /sc/api/gasleak/findbycondition ，在返回数据中提供门牌号等更多信息

--- sc-backend/src/main/java/com/sc/scbackend/SCPending/service/GasInfoService.java
+++ sc-backend/src/main/java/com/sc/scbackend/SCPending/service/GasInfoService.java
@@ -2,7 +2,14 @@ package com.sc.scbackend.SCPending.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.sc.scbackend.SCPending.domain.GasInfo;
+import com.sc.scbackend.SCPending.dto.GasPropertyInfoDTO;
+import com.sc.scbackend.SCPending.enums.GasStatus;
+import com.sc.scbackend.SCPending.enums.UrgencyLevel;
+
+import java.util.List;
 
 public interface GasInfoService extends IService<GasInfo> {
-    // 这里可以添加特定的方法
+    List<GasPropertyInfoDTO> getAllGasPropertyInfo();
+
+    List<GasPropertyInfoDTO> getAllGasPropertyInfoByConditions(UrgencyLevel urgencyLevel, GasStatus status);
 }
